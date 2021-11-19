3e630122e02731d951c2c8248d8dd02f2137e17d
jeffreyseger <jeffrey.seger@pangaea.nl>
2021-11-19 13:08:39 +0100

    Finished opdracht 1 , 2A & 2B

--- Les 2/2A/src/com/JeffDev/Les2.java
+++ Les 2/2A/src/com/JeffDev/Les2.java
@@ -2,19 +2,31 @@ package com.JeffDev;
 
 public class Les2 {
     public static void main(String[] arg) {
+        //create new zwembad z1.
         Zwembad z1 = new Zwembad(2.0, 5.5, 1.5);
+
+        //write values of zwembad z1.
         System.out.println("BREEDTE: " + z1.getBreedte() + " meter");
         System.out.println("LENGTE: " + z1.getLengte() + " meter");
         System.out.println("DIEPTE: " + z1.getDiepte() + " meter");
+
+        //write inhoud of zwembad z1.
         System.out.println("BEREKENDE INHOUD: " + z1.inhoud() + " liter");
         System.out.println();
 
+        //create new zwembad z2.
         Zwembad z2 = new Zwembad();
+
+        //set values of zwembad z2.
         z2.setBreedte(2.5);
         z2.setLengte(100.0);
         z2.setDiepte(2.0);
+
+        //get inhoud of zwembad z2.
         double inh = z2.inhoud();
-        System.out.println("GEGEVENS ZWEMBAD: " + z2.toString());
+
+        //z2 is enough to print all the info, since there is a toString value in the class.
+        System.out.println("GEGEVENS ZWEMBAD: " + z2);
         System.out.println("BEREKENDE INHOUD: " + inh  + " liter");
     }
 }
--- Les 2/2A/src/com/JeffDev/Zwembad.java
+++ Les 2/2A/src/com/JeffDev/Zwembad.java
@@ -1,45 +1,50 @@
 package com.JeffDev;
 
 public class Zwembad {
-    double breedte;
-    double lengte;
-    double diepte;
+    //zwembad values.
+    private double breedte;
+    private double lengte;
+    private double diepte;
 
+    //zwembad constructor, values from getters/setters.
     public Zwembad(){
 
     }
+    //zwembad constructor, direct values.
     public Zwembad(double breedte, double lengte, double diepte){
         setBreedte(breedte);
         setLengte(lengte);
         setDiepte(diepte);
     }
 
+    //Calculate inhoud value of zwembad.
     public double inhoud(){
         double inhoud = this.lengte * this.breedte * this.diepte * 1000;
         return inhoud;
     }
 
+    //create string to get info about zwembad.
     public String toString(){
         String info = this.breedte + " meter breedte, " + this.lengte + " meter lengte, " + this.diepte + " meter diepte.";
         return info;
     }
 
-    //getters and setters
-    //breedte
+    //getters and setters of zwembad values.
+    //breedte.
     public double getBreedte(){
         return this.breedte;
     }
     public void setBreedte(double breedte){
         this.breedte = breedte;
     }
-    //lengte
+    //lengte.
     public double getLengte(){
         return this.lengte;
     }
     public void setLengte(double lengte){
         this.lengte = lengte;
     }
-    //diepte
+    //diepte.
     public double getDiepte(){
         return this.diepte;
     }
